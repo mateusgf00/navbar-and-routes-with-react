c48bf92e1472b15cedad969ec4b900bc9eff73d9
betozanette <beto_zanette@hotmail.com>
2022-05-03 22:10:12 -0300

    fix: page height

--- src/screens/subsidiary/index.js
+++ src/screens/subsidiary/index.js
@@ -1,23 +1,20 @@
-import React, { Fragment } from "react";
-import { Column, Section, Title, Container, Card } from 'rbx';
+import React from "react";
+import { Column } from 'rbx';
 import SideBar from "../../components/sidebar";
 import SubsidiaryForm from "../../components/subsidiary";
 import './subsidiary.scss'
 
-function SubsidiaryScreen() {
-    return (
-        <Fragment>
-            <Column.Group centered id="columngroup" gapSize={8}>
-                <Column size={3} id="sidebar">
-                    <SideBar />
-                </Column>
-                <Column>
-                    <SubsidiaryForm />
-                </Column>
-            </Column.Group>
-        </Fragment>
-    )
-}
-
+const SubsidiaryScreen = () => (
+    <div style={{display: 'grid', height: '100%'}}>
+        <Column.Group centered id="columngroup" gapSize={8}>
+            <Column size={3} id="sidebar">
+                <SideBar />
+            </Column>
+            <Column>
+                <SubsidiaryForm />
+            </Column>
+        </Column.Group>
+    </div>
+);
 
 export default SubsidiaryScreen;
